eb3a22a31abc3831806c9ae7620f35c3fea7ffcf
dreyliba <libaandrey@gmail.com>
2025-10-31 22:00:53 +0800

    added Tabs status

--- app/Filament/Resources/Bookings/Pages/ListBookings.php
+++ app/Filament/Resources/Bookings/Pages/ListBookings.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\Bookings\Pages;
 
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\Bookings\BookingResource;
 
 class ListBookings extends ListRecords
@@ -14,6 +16,19 @@ class ListBookings extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'Pending' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'pending')),
+            'Confirmed' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'confirmed')),
+            'Cancelled' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'cancelled')),
         ];
     }
 }
